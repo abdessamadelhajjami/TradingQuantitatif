216c5d2ace10990c66142b52310dde0ecf86d922
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-02-22 11:28:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 import yfinance as yf
 import datetime
 
-# Télécharger les données du CAC40 des 6 derniers mois
+# Déterminer les dates de début et de fin pour les 6 derniers mois
 end_date = datetime.datetime.now()
 start_date = end_date - datetime.timedelta(days=6*30)
-cac40_data = yf.download("AAPL", start=30/12/2023, end=10/01/2024, progress=False)
+
+# Convertir les dates en chaînes de caractères dans le format 'YYYY-MM-DD'
+start_date_str = start_date.strftime('%Y-%m-%d')
+end_date_str = end_date.strftime('%Y-%m-%d')
+
+# Télécharger les données du CAC40 des 6 derniers mois
+cac40_data = yf.download("AAPL", start=2024/01/01, end=2024/01/10)
 
 # Créer l'application Streamlit
 st.title('Analyse du CAC40')
