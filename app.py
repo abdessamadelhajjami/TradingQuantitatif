eff144cb3d26ba7a8a4c7c7776c549640780b916
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-02-22 11:11:53 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import yfinance as yf
+import datetime
+
+# Télécharger les données du CAC40 des 6 derniers mois
+end_date = datetime.datetime.now()
+start_date = end_date - datetime.timedelta(days=6*30)
+cac40_data = yf.download('^FCHI', start=30/12/2023, end=20/02/2024)
+
+# Créer l'application Streamlit
+st.title('Analyse du CAC40')
+
+# Afficher les données dans un tableau
+st.write(cac40_data)
